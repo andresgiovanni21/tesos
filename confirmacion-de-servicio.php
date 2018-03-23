70b3d5bf6491a9d4b310db35e8249d3d0ecc0707
paolanatalia333 <paola@teratonsas.com>
2018-03-22 22:38:26 -0500

    prueba mail

--- confirmacion-de-servicio.php
+++ confirmacion-de-servicio.php
@@ -11,15 +11,18 @@ $x_ref_payco      = $_REQUEST['x_ref_payco'];
 $x_transaction_id = $_REQUEST['x_transaction_id'];
 $x_amount         = $_REQUEST['x_amount'];
 $x_currency_code  = $_REQUEST['x_currency_code'];
-x_extra1          = $_REQUEST['x_extra1'];
+$x_extra1          = $_REQUEST['x_extra1'];
 $x_signature      = $_REQUEST['x_signature'];
 $signature = hash('sha256', $p_cust_id_cliente . '^' . $p_key . '^' . $x_ref_payco . '^' . $x_transaction_id . '^' . $x_amount . '^' . $x_currency_code);
 $x_response     = $_REQUEST['x_response'];
 $x_motivo       = $_REQUEST['x_response_reason_text'];
 $x_id_invoice   = $_REQUEST['x_id_invoice'];
 $x_autorizacion = $_REQUEST['x_approval_code'];
-//Validamos la firma
+$email_body = "Bienvenido a tesos .$x_extra1\n";
+
+     //Validamos la firma
 if ($x_signature == $signature) {
+     mail( $_POST['andresgiovanni21@hotmail.com'], "Holiis", $email_body, 'From: ' . "pnati8@hotmail.com" );
     /*Si la firma esta bien podemos verificar los estado de la transacción*/
     $x_cod_response = $_REQUEST['x_cod_response'];
     switch ((int) $x_cod_response) {
